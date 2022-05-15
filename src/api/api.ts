c476e0b0ac649d2f93fdbb72dbedd26f3f553db8
AldairMC <aldairmc2023@gmail.com>
2022-05-14 21:14:13 -0500

    :rocket: Deploy ready!

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,16 +1,17 @@
 import axios from 'axios'
+const { REACT_APP_URI } = process.env
 
 export const getComment = async () => {
-    const { data } = await axios.get("https://fithubtest.free.beeceptor.com/reviews") 
+    const { data } = await axios.get(`${REACT_APP_URI}/reviews`) 
     return data
 }
 
 export const postLike = async (id: any) => {
-    const { data } = await axios.post(`https://fithubtest.free.beeceptor.com/reviews/${id}/like`)
+    const { data } = await axios.post(`${REACT_APP_URI}/reviews/${id}/like`)
     return data
 }
 
 export const postDislike = async (id: any) => {
-    const { data } = await axios.post(`https://fithubtest.free.beeceptor.com/reviews/${id}/dislike`)
+    const { data } = await axios.post(`${REACT_APP_URI}/reviews/${id}/dislike`)
     return data
 }
