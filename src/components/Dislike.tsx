fa0c17c0c57fad9729892fe431addafa2ea3a7ad
AldairMC <aldairmc2023@gmail.com>
2022-05-14 20:38:07 -0500

    :sparkles: Add Like and Dislike component

--- src/components/Dislike.tsx
+++ src/components/Dislike.tsx
@@ -1,7 +1,25 @@
-const Dislike = () => {
+import { useState } from "react";
+import { useMutation } from "react-query";
+import { postDislike } from "../api/api";
+
+type Props = {
+  id: string
+}
+
+const Dislike = ({ id }: Props) => {
+  const [ dislike, setDislike ] = useState(false)
+  const { mutate } = useMutation(() => postDislike(id))
+
+  const handleLike = () => {
+    mutate()
+    setDislike(!dislike)
+  }
+
   return (
-    <span>
-        <i className="fa-regular fa-thumbs-down"></i>
+    <span onClick={handleLike}>
+      {
+        dislike? <i className="fa-solid fa-thumbs-down"></i> : <i className="fa-regular fa-thumbs-down"></i>
+      }
     </span>
   )
 }
--- src/components/Like.tsx
+++ src/components/Like.tsx
@@ -1,11 +1,26 @@
+import { useState } from "react"
+import { useMutation } from "react-query";
+import { postLike } from "../api/api";
+
 type Props = {
     count: number
+    id: string
 }
 
-const Like = ({ count }: Props) => {
+const Like = ({ count, id }: Props) => {
+  const [ like, setLike ] = useState(false)
+  const { mutate } = useMutation(() => postLike(id))
+
+  const handleLike = () => {
+    mutate()
+    setLike(!like)
+  }
+
   return (
-    <span>
-        <i className="fa-regular fa-thumbs-up"></i> {count}
+    <span onClick={handleLike}>
+      {
+        like? <i className="fa-solid fa-thumbs-up"></i> : <i className="fa-regular fa-thumbs-up"></i>
+      } {count}
     </span>
   )
 }
